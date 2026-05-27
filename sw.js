8a7a55a51fa683aa6cb8eda07e685bf7efe77a67
TheKazutoPe <abrahamprado12345@gmail.com>
2026-05-27 01:37:11 -0500

    chore: revertir Fase 4 (Mapbox 3D) y limpiar cache del service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cicsa-gps-v1';
+const CACHE_NAME = 'cicsa-gps-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -8,11 +8,26 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Force new service worker to activate immediately
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
